d0ace6c0d29e5b4affa054462a792f600cc2e994
mazarafa <arruda.rafa.ra@gmail.com>
2019-11-18 16:15:11 -0300

    ingressos a venda no home, atualizadas hehe

--- views/listaEventos.php
+++ views/listaEventos.php
@@ -1,78 +1,33 @@
-<h2>Novidades</h2>
-                <br>
+<h2><?=$titulo?></h2>
+<!-- container de produtos -->
+<div class="lista-produtos">
 
-                <!-- container de produtos -->
-                <div class="lista-produtos">
-                    <!-- um produto -->
-                    <div class="produto">
-                        <a href="#">
-                            <figure>
-                                <img src="Img/vintage/she.jpg" alt="Rafis Chuchu">
-                                <figcaption>Rafis Chuchu
-                                    <br><span class="precoFinal">R$ 5,00</span>
-                                </figcaption>
-                            </figure>
-                        </a>
-                        <p class="rapida">compra rápida</p>
-                    </div>
-                    <!-- fim produto -->
-
-                    <!-- um produto -->
-                    <div class="produto">
-                        <a href="#">
-                            <figure>
-                                <img src="Img/vintage/aa.png" alt="Calourada de CC">
-                                <figcaption>Calourada de CC
-                                    <br><span class="precoFinal">R$ 10,00</span>
-                                </figcaption>
-                            </figure>
-                        </a>
-                        <p class="rapida">compra rápida</p>
-                    </div>
-                    <!-- fim produto -->
-
-                    <!-- um produto -->
-                    <div class="produto">
-                        <a href="#">
-                            <figure>
-                                <img src="Img/vintage/bb.png" alt="Open bar">
-                                <figcaption>Open bar
-                                    <br>De <span class="precoInicial">R$ 50,00</span> por <span class="precoFinal">R$ 30,00</span>
-                                </figcaption>
-                            </figure>
-                        </a>
-                        <p class="rapida">compra rápida</p>
-                    </div>
-                    <!-- fim produto -->
-
-                    <!-- um produto -->
-                    <div class="produto">
-                        <a href="#">
-                            <figure>
-                                <img src="Img/vintage/default.jpg" alt="False Alfter">
-                                <figcaption>False Alfter
-                                    <br><span class="precoFinal">R$ 60,00</span>
-                                </figcaption>
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- fim produto -->
-
-                    <!-- um produto -->
-                    <div class="produto">
-                        <a href="#">
-                            <figure>
-                                <img src="Img/vintage/life.jpg" alt="Tributo Avicci">
-                                <figcaption>Tributo Avicci
-                                    <br>De <span class="precoInicial">R$ 40,00</span> por <span class="precoFinal">R$ 20,00</span>
-                                </figcaption>
-                            </figure>
-                        </a>
-                        <p class="noCarrinho">no carrinho!</p>
-                    </div>
-                    <!-- fim produto -->
-
-                    <!-- adicione mais produtos -->
-
-                </div>
-                <!-- fim lista produtos -->
+    <?php
+    if(empty($listas)) {
+        echo "<p>Nenhum produto encontrado</p>";
+    }
+    else {
+        #$linha = $consulta->fetch(PDO::FETCH_ASSOC)
+        foreach($listas as $v => $lista){
+        ?>
+        
+        <!-- um produto -->
+        <div class="produto">    
+            <a href="evento.php?id=<?=$lista['id'];?>">                  
+                <figure>                           
+                    <img src="Img/vintage/<?=mostraImagem($lista['imagem']);?>" alt="<?=$lista['nome'];?>">
+                    <figcaption><?=$lista['nome'];?>
+                    <br>
+                    <span class="precoFinal">
+                        <?=formataPreco($lista['valor']);?>
+                    </span>
+                    </figcaption>                          
+                </figure>      
+            </a>              
+            <p class="rapida">compra rápida</p>
+        </div>   
+        <!-- fim produto -->       
+        <?php  
+        }
+    }
+    ?>
